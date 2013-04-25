f17d785081657aa58e76d26d631a4eee8e6882e5
Phil Booth <pmbooth@gmail.com>
2013-04-25 14:24:55 +0100

    Ooify the exports.

--- src/safemap.js
+++ src/safemap.js
@@ -1,89 +1,80 @@
-// A tiny, safe-for-all-property-names map/dictionary implementation in JavaScript.
-
 /*globals define, module */
 
 (function (globals) {
     'use strict';
 
-    var functions = {
-        get: get,
-        set: set,
-        has: has,
-        // Opted for `remove` rather than the WeakMap-like `delete`
-        // to ease burden on callers in ES3 environments.
-        remove: remove,
-        clear: clear,
-        safeGet: safeGet,
-        safeSet: safeSet,
-        safeRemove: safeRemove
-    };
+    var hasOwnProperty = Object.prototype.hasOwnProperty;
 
-    // Public function `get`.
+    // Object `SafeMap`.
     //
-    // Returns the value associated with `key`, if `key` is in the map.
-    // If `key` is not in the map, returns `defaultValue` (i.e. returns
-    // `undefined` if no defaultValue argument is provided by the caller).
-    function get (key, defaultValue) {
-    }
+    // A tiny, safe-for-all-property-names
+    // map/dictionary implementation.
+    function SafeMap () {
+        var map = {};
 
-    // Public function `set`.
-    //
-    // Sets a value to be associated with `key`, over-writing any former
-    // value that may have been set for `key`.
-    function set (key, value) {
-    }
-
-    // Public function `has`.
-    //
-    // Returns a boolean indicating whether `key` is in the map.
-    function has (key) {
-    }
-
-    // Public function `remove`.
-    //
-    // Removes `key` from the map.
-    function remove (key) {
-    }
-
-    // Public function `clear`.
-    //
-    // Removes all keys from the map.
-    function clear () {
-    }
-
-    // Public function `safeGet`.
-    //
-    // Throwing version of `get`. No default value can be specified and,
-    // if `key` is not in the map, an Error will be thrown.
-    function safeGet (key) {
-    }
-
-    // Public function `safeSet`.
-    //
-    // Throwing version of `set`. If `key` is already in the map, an
-    // Error will be thrown.
-    function safeSet (key, value) {
-    }
-
-    // Public function `safeRemove`.
-    //
-    // Throwing version of `remove`. If `key` is not in the map, an
-    // Error will be thrown.
-    function safeRemove (key) {
-    }
-
-    exportFunctions();
-
-    function exportFunctions () {
-        if (typeof define === 'function' && define.amd) {
-            define(function () {
-                return functions;
-            });
-        } else if (typeof module === 'object' || module !== null) {
-            module.exports = functions;
-        } else {
-            globals.check = functions;
+        // Public method `get`.
+        //
+        // Returns the value associated with `key`, if `key` is in the map.
+        // If `key` is not in the map, returns `defaultValue` (i.e. returns
+        // `undefined` if no defaultValue argument is provided by the caller).
+        this.get = function (key, defaultValue) {
         }
+
+        // Public method `set`.
+        //
+        // Sets a value to be associated with `key`, over-writing any former
+        // value that may have been set for `key`.
+        this.set = function (key, value) {
+        }
+
+        // Public method `has`.
+        //
+        // Returns a boolean indicating whether `key` is in the map.
+        this.has = function (key) {
+        }
+
+        // Public method `remove`.
+        //
+        // Removes `key` from the map.
+        this.remove = function (key) {
+        }
+
+        // Public method `clear`.
+        //
+        // Removes all keys from the map.
+        this.clear = function () {
+        }
+
+        // Public method `safeGet`.
+        //
+        // Throwing version of `get`. No default value can be specified and,
+        // if `key` is not in the map, an Error will be thrown.
+        this.safeGet = function (key) {
+        }
+
+        // Public method `safeSet`.
+        //
+        // Throwing version of `set`. If `key` is already in the map, an
+        // Error will be thrown.
+        this.safeSet = function (key, value) {
+        }
+
+        // Public method `safeRemove`.
+        //
+        // Throwing version of `remove`. If `key` is not in the map, an
+        // Error will be thrown.
+        this.safeRemove = function (key) {
+        }
+    }
+
+    if (typeof define === 'function' && define.amd) {
+        define(function () {
+            return SafeMap;
+        });
+    } else if (typeof module === 'object' || module !== null) {
+        module.exports = SafeMap;
+    } else {
+        globals.SafeMap = SafeMap;
     }
 }(this));
 
